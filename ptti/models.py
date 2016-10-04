22b9100a0a622277b1a0401c50c73a7d93b7376f
Carlos Gutierrez <rapsta93@gmail.com>
2016-10-04 17:51:13 -0500

    add reqs.txt, edit models

--- ptti/models.py
+++ ptti/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
-#instiuciones
+#modelo de instiuciones
 class Institucion(models.Model):
     nit         = models.CharField(max_length=200,unique=True)
     nombre      = models.CharField(max_length=200)
@@ -18,6 +18,11 @@ class Institucion(models.Model):
     def __str__(self):
         return self.nombre
 
+    class Meta:
+        verbose_name = "Institucion"
+        verbose_name_plural = "Instituciones"
+
+#modelo de grupos
 class Grupo(models.Model):
     JORNADA_LIST = [
         ('M', 'MANANA'),
@@ -55,11 +60,18 @@ class Grupo(models.Model):
     def __str__(self):
         return self.nombre + "-" + self.institucion.nombre
 
-#usuarios
+    class Meta:
+        verbose_name = "Grupo"
+        verbose_name_plural = "Grupos"
+
+#modelo de usuarios
 class Usuario(AbstractUser):
     no_docto    = models.CharField(max_length=64)
+
     class Meta:
         db_table = 'auth_user'
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
 
 class Administrador(models.Model):
     user        = models.OneToOneField(Usuario, on_delete=models.CASCADE)
@@ -75,18 +87,60 @@ class Estudiante(models.Model):
     institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
     grupo       = models.ForeignKey(Grupo, on_delete=models.CASCADE)
 
+#modelo de Test
 class TestTI(models.Model):
     nombre      = models.CharField(max_length=200)
 
-class PreguntasTestTI(models.Model):
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Test TI"
+        verbose_name_plural = "Tests TI"
+
+class PreguntaTestTI(models.Model):
     test        = models.ForeignKey(TestTI, on_delete=models.CASCADE)
     pregunta    = models.CharField(max_length=200)
     numero      = models.IntegerField(unique=True)
 
-class RespuestasTestTI(models.Model):
-    pregunta    = models.ForeignKey(PreguntasTestTI, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.numero + " " +  self.pregunta
+
+    class Meta:
+        verbose_name = "Pregunta Test TI"
+        verbose_name_plural = "Preguntas Test TI"
+
+class RespuestaTestTI(models.Model):
+    pregunta    = models.ForeignKey(PreguntaTestTI, on_delete=models.CASCADE)
     respuesta   = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.respuesta
+
+    class Meta:
+        verbose_name = "Respuesta Test TI"
+        verbose_name_plural = "Respuestas Test TI"
+
+#modelo de respuestas a los test
 class TestAsignado(models.Model):
     estudiante  = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
     test        = models.ForeignKey(TestTI, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.test + " " + self.estudiante
+
+    class Meta:
+        verbose_name = "Test Asignado"
+        verbose_name_plural = "Tests Asignados"
+
+class RespuestaEstudiante(models.Model):
+    test        = models.ForeignKey(TestAsignado, on_delete=models.CASCADE)
+    pregunta    = models.ForeignKey(PreguntaTestTI, on_delete=models.CASCADE)
+    respuesta   = models.ForeignKey(RespuestaTestTI, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.test + " " + self.estudiante
+
+    class Meta:
+        verbose_name = "Test Asignado"
+        verbose_name_plural = "Tests Asignados"
